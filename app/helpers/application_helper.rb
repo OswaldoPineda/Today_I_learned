e0bcd08d7b403fc109eaa3390c828c3bf836f09b
Oswaldo Pineda <carlos-1502010@hotmail.com>
2022-03-28 00:16:14 -0600

    Add Makdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def markdown(text)
+    options = {
+      hard_wrap: true,
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
