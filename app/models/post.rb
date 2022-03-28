180bda7da30cb4617869f0b0e7b138e6ca524479
Oswaldo Pineda <carlos-1502010@hotmail.com>
2022-03-27 23:17:36 -0600

    Add user to post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title,  :presence => true
   validates :content, :presence => true,
                     :length => { :minimum => 5 }
+  belongs_to :user
 end
