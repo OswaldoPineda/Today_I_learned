a58a430d9903389d8e012c2ec8ff3cb0ed219efd
Oswaldo Pineda <carlos-1502010@hotmail.com>
2022-04-26 17:56:48 -0500

    Install linters and fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
 end
