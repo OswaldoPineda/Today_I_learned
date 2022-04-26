038982198b62c7d6669504ecce833dbf2f7e52fa
Oswaldo Pineda <consultant-opineda@remine.com>
2022-04-26 17:56:48 -0500

    Install linters and fix errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-  validates :title,  :presence => true
-  validates :content, :presence => true,
-                    :length => { :minimum => 5 }
+  validates :title, presence: true
+  validates :content, presence: true,
+                      length: { minimum: 5 }
   belongs_to :user
 end
