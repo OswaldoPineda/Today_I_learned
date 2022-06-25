949496d8644374b13e473cf0fb263614d6830d4d
Oswaldo Pineda <carlos-1502010@hotmail.com>
2022-06-24 22:49:34 -0500

    Add Labels to Posts relationship

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Label < ApplicationRecord
+  validates :name, presence: true
+  has_and_belongs_to_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_23_211337) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_214339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_211337) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "labels_posts", force: :cascade do |t|
+    t.bigint "label_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label_id"], name: "index_labels_posts_on_label_id"
+    t.index ["post_id"], name: "index_labels_posts_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -41,5 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_23_211337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "labels_posts", "labels"
+  add_foreign_key "labels_posts", "posts"
   add_foreign_key "posts", "users"
 end
