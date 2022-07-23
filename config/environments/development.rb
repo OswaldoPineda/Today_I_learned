8a659e73e05551e81a1d252f6b7bc6ad411af6fa
Oswaldo Pineda <carlos-1502010@hotmail.com>
2022-07-22 21:12:24 -0500

    Add labels to Posts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,10 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.action_cable.url = "ws://localhost:3000/cable"
+
+  config.action_cable.allowed_request_origins = [/http:\/\/*/,
+                                                 /https:\/\/*/]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
