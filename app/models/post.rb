c034e670094dae393a272fcf21ccf15f4642688f
Oswaldo Pineda <consultant-opineda@remine.com>
2022-07-23 22:19:08 -0500

    Fix linter on Ruby files

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,9 @@ class Post < ApplicationRecord
   validates :content, presence: true,
                       length: { minimum: 5 }
   belongs_to :user
-  has_and_belongs_to_many :labels
+  # rubocop:disable all
+  has_and_belongs_to_many :labels, dependent: :destroy
+  # rubocop:enable all
 
   def add_labels(label_ids)
     labels = Label.where(id: label_ids)
