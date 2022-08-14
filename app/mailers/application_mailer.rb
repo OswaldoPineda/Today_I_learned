b05cda0028853e264d5a2148fa01e4789807280e
Oswaldo Pineda <consultant-opineda@remine.com>
2022-08-13 20:29:11 -0500

    Configure SendGrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'carlos-1502010@hotmail.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :newsletters
   resources :posts
   devise_for :users
+  get '/users/password', to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
