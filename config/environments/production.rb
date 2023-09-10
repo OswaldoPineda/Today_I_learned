92215348f4cc82b241a12c17e899189123d77980
Oswaldo Pineda <carlos-1502010@hotmail.com>
2023-09-10 12:19:42 -0600

    Update action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "today_i_learned_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'today-i-learned-app-production.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.til-dev.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       user_name: 'apikey',
       password: ENV['SENDGRID_API_KEY'],
-      domain: 'herokuapp.com',
+      domain: 'til-dev.com',
       address: 'smtp.sendgrid.net',
       port: 587,
       authentication: :plain,
