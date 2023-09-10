752d1be2ecc45262965e06c3a2f3df99cfc0aee1
Oswaldo Pineda <carlos-1502010@hotmail.com>
2023-09-10 14:48:42 -0600

    Add test to validate uniq email and username

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -2,7 +2,8 @@
 
 require 'rails_helper'
 
-describe 'Sign un', type: :feature do
+# rubocop:disable Metrics/BlockLength
+describe 'Sign up', type: :feature do
   before :each do
     @user = FactoryBot.build(:user)
     visit '/users/sign_up'
@@ -29,4 +30,34 @@ describe 'Sign un', type: :feature do
     click_button 'Sign up'
     expect(page).to have_content "Email can't be blank"
   end
+
+  context 'signs me in' do
+    before :each do
+      @user = FactoryBot.create(:user)
+    end
+
+    it 'with repeated email' do
+      within('#new_user') do
+        fill_in 'Username', with: 'Test Email2'
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: @user.password
+        fill_in 'Confirm Password', with: @user.password
+      end
+      click_button 'Sign up'
+      expect(page).to have_content 'Email has already been taken'
+    end
+
+    it 'with repeated username' do
+      within('#new_user') do
+        fill_in 'Username', with: @user.username
+        fill_in 'Email', with: 'usertest@test.com'
+        fill_in 'Password', with: @user.password
+        fill_in 'Confirm Password', with: @user.password
+      end
+      click_button 'Sign up'
+      expect(page).to have_content 'Username has already been taken'
+    end
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
