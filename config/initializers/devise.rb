c88cdab072d5ce03ef2b91924c45599099d41b7a
Oswaldo Pineda <consultant-opineda@remine.com>
2024-01-02 19:43:14 -0600

    Fix default email from Devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'carlos-1502010@hotmail.com'
+  config.mailer_sender = 'no-reply@tildev.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
