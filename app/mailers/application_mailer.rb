1d6c1b8575d515f5787e3829a00b0351d3c5665b
Oswaldo Pineda <carlos-1502010@hotmail.com>
2024-01-04 18:11:54 -0600

    Revert "Change default email"

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@tildev.com'
+  default from: 'carlos-1502010@hotmail.com'
   layout 'mailer'
 end
