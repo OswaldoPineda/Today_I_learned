23cf271cfea4ed9bc8cf3cf01e3709f2b8dbd5bc
Juanequex <ramon.g.uz37@gmail.com>
2024-01-06 13:13:25 -0600

    remove clipboard congifuration from application file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,39 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "clipboard";
 import "controllers";
 import "trix"
 import "@rails/actiontext"
-
-
-import ClipboardJS from 'clipboard';
-var copyButtons = document.querySelectorAll('[id="copyPathButton"]');
-
-copyButtons.forEach(function(copyButton) {
-  var clipboard = new ClipboardJS(copyButton);
-
-  clipboard.on('success', function(e) {
-    var cardContainer = e.trigger.closest('.card-container');
-    showAlert('Post URL copied to clipboard!', cardContainer);
-    e.clearSelection();
-  });
-
-  clipboard.on('error', function(e) {
-    showAlert('Error copying path to clipboard. Please try again.');
-  });
-
-  copyButton.addEventListener('click', function(event) {
-    event.preventDefault();
-  });
-});
-
-function showAlert(message, container) {
-  var alertDiv = document.createElement('div');
-  alertDiv.textContent = message;
-  alertDiv.classList.add('alert-clipboard');
-
-  container.appendChild(alertDiv);
-
-  setTimeout(function() {
-    alertDiv.remove();
-  }, 2000);
-}
+import "clipboard_config"
