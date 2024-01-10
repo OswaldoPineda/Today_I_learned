cf65e7b59c4c4b5d8c149f0db9b3d7a5fd0b103c
Oswaldo Pineda <carlos-1502010@hotmail.com>
2024-01-10 17:19:50 -0600

    Add pagination to users posts page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
     markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     sanitize(markdown_to_html.render(text))
   end
+
+  def show_pagination
+    is_homepage = request.path == '/'
+    is_posts = (request.path == '/posts' && params[:action] == 'index')
+    is_users_posts = request.path.match?(%r{^/users/\d+/posts$})
+
+    is_homepage || is_posts || is_users_posts
+  end
 end
