1cc4978eaa9037121dd8d46645c2380db589b165
Oswaldo Pineda <carlos-1502010@hotmail.com>
2024-01-17 21:07:02 -0600

    Update newsletter mailer to select only active subscriptions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@
 
 class UserMailer < ApplicationMailer
   def newsletter_mailer
-    @newsletter = Newsletter.all
+    @newsletter = Newsletter.where('subscription = ?', true)
     @post = Post.order('RANDOM()').limit(5)
     emails = @newsletter.collect(&:email).join(", ")
     mail(to: emails, subject: "Weekly Today I Learn Posts.")
